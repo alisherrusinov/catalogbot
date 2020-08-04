39e87f38c139569a1249072d955ea50d2faa358f
DanteXGit <alisher.nugmanoff@yandex.ru>
2020-08-04 18:56:10 +0500

    Changed library to aiogram

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
-import telebot
-from telebot import types
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 from config import TOKEN
 
-bot = telebot.TeleBot(TOKEN)
-ADMINS = [
-    834035462,
-]
-@bot.message_handler(commands=['start','help'])
-def send_welcome(message):
-    keyboard = types.ReplyKeyboardMarkup()
-    ctlgbtn = types.InlineKeyboardButton(text='Категории')
-    keyboard.add(ctlgbtn)
-    bot.send_message(
-        chat_id=message.chat.id,
-        text='''
-        Привет!\nЯ - бот, в которого можно загрузить каталог товаров с сайта, чтобы клиенты могли смотреть его прямо в телеграмме(в данной версии пока что нет оплаты, только просмотр товаров)
-        ''',
-        reply_markup=keyboard,
-        )
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-@bot.message_handler(content_types=['text',])
-def get_word(message):
-    if(message.text.startswith('Что такое',0,9)):
-        try:
-            search = wikipedia.page(message.text.replace('Что такое ',''))
-            answer = search.content[:300]
-            keyboard = types.InlineKeyboardMarkup()
-            button = types.InlineKeyboardButton(
-                text='Ссылка на статью',
-                url=search.url
-            )
-            keyboard.add(button)
-            bot.send_message(
-            chat_id=message.chat.id,
-            text=answer,
-            reply_markup=keyboard
-        )
-        except wikipedia.exceptions.DisambiguationError as error:
-            bot.send_message(
-                chat_id=message.chat.id,
-                text=error
-            )
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
 
-bot.polling()
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
--- models.py
+++ models.py
@@ -1,26 +1,24 @@
 #File need to be runned once!
 import sqlite3
 
-class Product:
+class DBManager:
     def __init__(self):
         """Подключение к БД и создание полей"""
 
         self.connection = sqlite3.connect('db.db')
-        self.cursor = self.connection.cursor()
-
-        fields = self.get_product('Товар')
-        self.name = fields[0][1]
-        self.desc = fields[0][2]
-        self.img = fields[0][3]
-        self.category = fields[0][4]
-        self.cost = fields[0][5]
-        self.page = fields[0][6]
     
-    def get_product(self,product):
+    def get_product(self, product):
         """Получить товар из базы данных"""
         with self.connection:
-            return self.cursor.execute(f"SELECT * FROM products WHERE name = '{product}';").fetchall()
-
-
-a = Product()
-print(a.category)
+            cur = self.connection.cursor()
+            return cur.execute(f"SELECT * FROM products WHERE name = '{product}';").fetchall()
+    def get_from_category(self, category):
+        """Получить все товары из категории"""
+        with self.connection:
+            rcur = self.connection.cursor()
+            return cur.execute(f"SELECT name FROM products WHERE category = '{category}';").fetchall()
+    def get_categories(self):
+        """Получить все категории товаров"""
+        with self.connection:
+            cur = self.connection.cursor()
+            return cur.execute("SELECT category FROM products;").fetchall()
