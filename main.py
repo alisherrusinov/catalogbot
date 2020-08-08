83604fd5c61a732de4046addf17538621a5ffd2d
DanteXGit <alisher.nugmanoff@yandex.ru>
2020-08-08 23:45:12 +0500

    database remaster

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 from config import TOKEN
+from models import DBManager
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
+db = DBManager()
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
@@ -17,6 +18,11 @@ async def process_start_command(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+@dp.message_handler(commands=['catalog'])
+async def process_help_command(message: types.Message):
+    categories = db.get_categories()
+    await bot.send_message(msg.from_user.id, msg.text)
 
 
 @dp.message_handler()
--- models.py
+++ models.py
@@ -4,21 +4,35 @@ import sqlite3
 class DBManager:
     def __init__(self):
         """Подключение к БД и создание полей"""
-
         self.connection = sqlite3.connect('db.db')
+        self.cursor = self.connection.cursor()
     
     def get_product(self, product):
         """Получить товар из базы данных"""
         with self.connection:
-            cur = self.connection.cursor()
-            return cur.execute(f"SELECT * FROM products WHERE name = '{product}';").fetchall()
+            query = self.cursor.execute(f"SELECT * FROM products WHERE name = '{product}';").fetchall()
+            return list(query[0])
     def get_from_category(self, category):
         """Получить все товары из категории"""
         with self.connection:
-            rcur = self.connection.cursor()
-            return cur.execute(f"SELECT name FROM products WHERE category = '{category}';").fetchall()
+            query = self.cursor.execute(f"SELECT name FROM products WHERE category = '{category}';").fetchall()
+            products = []
+            for i in query:
+                products.append(i[0])
+            return products
     def get_categories(self):
         """Получить все категории товаров"""
         with self.connection:
-            cur = self.connection.cursor()
-            return cur.execute("SELECT category FROM products;").fetchall()
+            query = self.cursor.execute("SELECT category FROM products;").fetchall()
+            categories = []
+            for i in query:
+                categories.append(i[0])
+            return categories
+    def get_products(self):
+        """Получить имена всех товаров"""
+        with self.connection:
+            query = self.cursor.execute("SELECT name FROM products;").fetchall()
+            products = []
+            for i in query:
+                products.append(i[0])
+            return products
