ad209f8aea93d0042fd4a5c86a68aa3f4286b145
DanteXGit <alisher.nugmanoff@yandex.ru>
2020-08-09 23:38:53 +0500

    added getting all products from category

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ db = DBManager()
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
-
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
@@ -26,15 +25,29 @@ async def process_help_command(msg: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     for cat in categories:
         keyboard.add(
-            InlineKeyboardButton(text=cat,callback_data='azaza')
+            InlineKeyboardButton(
+                text=cat,
+                callback_data=f'ctg_{cat}')
         )
     await bot.send_message(msg.from_user.id,'Категории:',reply_markup=keyboard)
-
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
 
-
+@dp.callback_query_handler(lambda call: call.data and call.data.startswith('ctg_'))
+async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+    """Выбирает все товары из определенной категории"""
+    query = callback_query.data.replace('ctg_','')
+    products = db.get_from_category(query)
+    keyboard = types.InlineKeyboardMarkup()
+    for product in products:
+        keyboard.add(
+            InlineKeyboardButton(
+                text=product,
+                callback_data=f'prdct_{product}'
+            )
+        )
+    await bot.send_message(callback_query.from_user.id,f'{query}:',reply_markup=keyboard)
 if __name__ == '__main__':
     executor.start_polling(dp)
